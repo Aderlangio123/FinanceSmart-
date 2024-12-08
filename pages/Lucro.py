e426dd9a2309ffaa25091a4aea2007ae9b1d6f4c
Aderlangio123 <faderlangio123@gmail.com>
2024-12-08 12:17:18 -0300

    atu final

--- pages/Lucro.py
+++ pages/Lucro.py
@@ -1,23 +1,23 @@
 import streamlit as st
 import base64
 from datetime import datetime
+import pytz  # Para manipulação de fuso horário
 
 st.sidebar.image("img/logo-empresa.png")
 
 def verificar_autenticacao():
-     if "email" not in st.session_state or "nome_de_usuario" not in st.session_state or "senha" not in st.session_state:
-         st.write("Você precisa se cadastrar primeiro para acessar esta página.")
-         st.write("Por favor, vá até a página de cadastro para continuar.")
-         if st.button("ir para cadastro"):
-             st.switch_page("pages/cadastro.py")
-         return False 
-     return True 
+    if "email" not in st.session_state or "nome_de_usuario" not in st.session_state or "senha" not in st.session_state:
+        st.write("Você precisa se cadastrar primeiro para acessar esta página.")
+        st.write("Por favor, vá até a página de cadastro para continuar.")
+        if st.button("ir para cadastro"):
+            st.switch_page("pages/cadastro.py")
+        return False 
+    return True 
 
 def lucros():
     if not verificar_autenticacao(): 
-         return 
+        return 
 
-    
     def image_to_base64(image_path):
         with open(image_path, "rb") as img_file:
             return base64.b64encode(img_file.read()).decode('utf-8')
@@ -35,7 +35,6 @@ def lucros():
                 background-attachment: fixed;  /* A imagem fica fixa enquanto rola */
                 height: 100vh;  /* Garante que a imagem ocupe toda a altura da tela */
                 margin: 0;
-
             }}
 
             .content {{
@@ -62,8 +61,7 @@ def lucros():
 
     valor_lucro = st.number_input("Informe o valor do lucro")
 
-
-    nome_lucro = st.text_input(" Nome ou descrição do lucro:")
+    nome_lucro = st.text_input("Nome ou descrição do lucro:")
 
     categorias = [
         "Vendas de Produtos", 
@@ -73,19 +71,22 @@ def lucros():
         "Salário Mensal", 
         "Outros Lucros"
     ]
-    categoria_lucro = st.selectbox("Escolha a categoria da despesa", categorias)
+    categoria_lucro = st.selectbox("Escolha a categoria do lucro", categorias)
 
     if st.button("Adicionar lucro", key="button-lucro"):
         if valor_lucro > 0 and nome_lucro.strip() != "" and categoria_lucro != "":
-            horario_adicao = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            # Ajustando para o fuso horário correto
+            fuso_horario = pytz.timezone("America/Sao_Paulo")
+            horario_adicao = datetime.now(pytz.utc).astimezone(fuso_horario).strftime("%Y-%m-%d %H:%M:%S")
+            
             lucro = {
-                 "nome": nome_lucro, 
-                 "valor": valor_lucro,
-                 "categoria": categoria_lucro,
-                 "hora": horario_adicao
-                 }
+                "nome": nome_lucro, 
+                "valor": valor_lucro,
+                "categoria": categoria_lucro,
+                "hora": horario_adicao
+            }
             st.session_state.lucros.append(lucro)
-            st.success(f"Lucro : {nome_lucro } de R${valor_lucro:.2f} na categoria '{categoria_lucro} no horário {horario_adicao} adicionado!")
+            st.success(f"Lucro: {nome_lucro} de R${valor_lucro:.2f} na categoria '{categoria_lucro}' no horário {horario_adicao} adicionado!")
         else:
             st.warning("Por favor, preencha todos os campos corretamente.")
 
